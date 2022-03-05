e6fc2702f9e994f2c4d83643949fc385db3fe446
zhengshunze <zhengshunze2020@gmail.com>
2022-03-04 18:54:20 -0800

    OK

---
+++ Speech/Speech-Bridging-Header.h
@@ -0,0 +1,6 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <googleapis/google/cloud/speech/v1/CloudSpeech.pbobjc.h>
+#import <googleapis/google/cloud/speech/v1/CloudSpeech.pbrpc.h>
